f1e7f84ae210d9d068b0ab89dc5c22fca30c5b81
Deckel De lange <deckeldelange@Deckels-MacBook-Pro.local>
2022-06-14 20:58:55 +0100

    init_commit

---
+++ city_mapper_api.py
@@ -0,0 +1,17 @@
+import requests
+from config import secret_key
+
+class CityMapper:
+  	def __init__(self, secret_key, end_point):
+  		self.secret_key = secret_key
+  		self.end_point = end_point
+
+  	def travel_time(start, end):
+  		requests.get(
+  			self.end_point + '1/traveltimes',
+  			params={'secret_key': self.secret_key}
+  			)
+
+
+
+city_mapper = CityMapper(secret_key, 'https://api.external.citymapper.com/api/')
---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+
+def main():
+	print("hello")
+
+
+if __name__ == '__main__':
+	main()
